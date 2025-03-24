5d2fdf031545b2e7210b7d5271f086aef90739dd
kimbro97 <rlagudwog@gmail.com>
2025-03-24 17:27:00 +0900

    feat: 회원 포인트내역 조회 API 기능 작업 및 테스트코드 작성

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,7 +1,10 @@
 package io.hhplus.tdd.point;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 
@@ -10,8 +13,13 @@ import lombok.RequiredArgsConstructor;
 public class PointService {
 
 	private final UserPointTable userPointTable;
+	private final PointHistoryTable pointHistoryTable;
 
 	public UserPoint getUserPoint(long id) {
 		return userPointTable.selectById(id);
+	}
+
+	public List<PointHistory> getPointHistory(long userId) {
+		return pointHistoryTable.selectAllByUserId(userId);
 	}
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -3,12 +3,15 @@ package io.hhplus.tdd.point;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,6 +19,9 @@ class PointServiceTest {
 
 	@Mock
 	private UserPointTable userPointTable;
+
+	@Mock
+	private PointHistoryTable pointHistoryTable;
 
 	@InjectMocks
 	private PointService pointService;
@@ -30,5 +36,23 @@ class PointServiceTest {
 
 		assertThat(userPoint.point()).isEqualTo(300);
 
+	}
+
+	@Test
+	void userId_값으로_포인트_내역을_조회할_수_있다() {
+		when(pointHistoryTable.selectAllByUserId(1))
+			.thenReturn(createPointHistoryList());
+
+		List<PointHistory> pointHistory = pointService.getPointHistory(1);
+
+		assertThat(pointHistory).hasSize(3);
+	}
+
+	private List<PointHistory> createPointHistoryList() {
+		return List.of(
+			new PointHistory(1, 1, 3000, TransactionType.CHARGE, System.currentTimeMillis()),
+			new PointHistory(2, 1, 2000, TransactionType.USE, System.currentTimeMillis()),
+			new PointHistory(3, 1, 3500, TransactionType.CHARGE, System.currentTimeMillis())
+		);
 	}
 }
