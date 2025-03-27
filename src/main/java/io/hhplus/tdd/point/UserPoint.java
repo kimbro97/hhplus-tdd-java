820a079fef926918dc90e24353c456deb7fb24b7
kimbro97 <rlagudwog@gmail.com>
2025-03-27 14:25:58 +0900

    feat: PointService 기능작업

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -1,12 +1,44 @@
 package io.hhplus.tdd.point;
 
 public record UserPoint(
-        long id,
-        long point,
-        long updateMillis
+	long id,
+	long point,
+	long updateMillis
 ) {
 
-    public static UserPoint empty(long id) {
-        return new UserPoint(id, 0, System.currentTimeMillis());
-    }
+	private static final long MAX_USER_POINT = 10000;
+	private static final long MIN_POINT_AMOUNT = 0;
+
+	public static UserPoint empty(long id) {
+		return new UserPoint(id, 0, System.currentTimeMillis());
+	}
+
+	public long increasePoint(long amount) {
+		return this.point + amount;
+	}
+
+	public long decreasePoint(long amount) {
+		return this.point - amount;
+	}
+
+	public void chargeValidate(long amount) {
+
+		if (amount <= MIN_POINT_AMOUNT) {
+			throw new IllegalArgumentException("충전 포인트는 0보다 커야합니다.");
+		}
+
+		if (this.point + amount > MAX_USER_POINT) {
+			throw new IllegalArgumentException("포인트는 10000원을 초과할 수 없습니다.");
+		}
+	}
+
+	public void useValidate(long amount) {
+		if (amount <= 0) {
+			throw new IllegalArgumentException("사용 포인트는 0보다 커야합니다.");
+		}
+
+		if (this.point < amount) {
+			throw new IllegalArgumentException("보유 포인트가 부족합니다.");
+		}
+	}
 }
